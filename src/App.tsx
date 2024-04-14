ddfd1148268919e91b4a10936838adcb2c00d8ce
Joanna Hulek <joanna.hulek@gmail.com>
2024-04-14 13:52:24 +0200

    items added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import {Fragment, useState} from "react";
+import {useState} from "react";
 import ListGroup from "./components/ListGroup.tsx";
 import Alert from "./components/Alert.tsx";
 import Button from "./components/Button.tsx";
+import Navbar from "./components/Navbar.tsx";
+import AboutMe from "./components/Bio.tsx";
+import Message from "./components/Message.tsx";
 
 
 
 function App(){
     const [alertVisable, setAlertVisable] = useState(false)
 
-    return (<>
+    return (<div>
+        {Navbar()}
+        {Message()}
         {alertVisable && <Alert onClose={()=>setAlertVisable(false)}>I TOLD YOU!</Alert>}
         <Button color='secondary' onClick={()=>setAlertVisable(true)}>DO NOT CLICK ME!</Button>
         {renderList()}
-    </>)
+        <section id="bio">{AboutMe()}</section>
+        </div>)
 }
 
 
